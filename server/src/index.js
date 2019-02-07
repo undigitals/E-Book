fe3d9d798729c88684d3a4e81b69906dab6c23a7
Umeed Negmatullayev <umeednegmatullayev@baggyeongsigs-MacBook-Pro.local>
2019-02-07 20:51:15 +0900

    Connecting to Database

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,21 @@
+
 const { ApolloServer } = require('apollo-server');
+
+// to create SQL database
+const { createStore } = require('./utils');
+
 const typeDefs = require('./schema');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store =  createStore();
 
 const server = new ApolloServer({ 
-  typeDefs
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  })
 });
 
 server.listen().then(({ url }) => {
